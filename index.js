bf669d4c61bbace2ae7cce888f5eea03d1a6fd17
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-11-22 23:01:55 +0530

    add start script

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
 
+
+// import routs
+const authRoute = require('./router/auth')
+
+// route middleware
+app.use('/api/user',authRoute)
+
 app.listen(3000,()=>console.log('server started.'))
